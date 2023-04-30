7abe3ca9f1258d5c27c8708c94eb56c7216b688e
kevinbriceo567 <kevinbriceo567@gmail.com>
2023-04-29 22:35:00 -0400

    Display basic information

--- app/page.tsx
+++ app/page.tsx
@@ -11,21 +11,114 @@ const octokit = new Octokit({
   auth: process.env.GITHUB_API_KEY
 });
 
+interface response_data {
+  author: author;
+  comments_url: string;
+  commit: commit;
+  committer: commiter;
+  html_url: string;
+  node_id: string;
+  parents: [parent];
+  sha: string;
+  url: string;
+}
+
+interface author {
+  avatar_url: string;
+  events_url: string;
+  followers_url: string;
+  following_url: string;
+  gists_url: string;
+  gravatar_id: string;
+  html_url: string;
+  id: number;
+  login: string;
+  node_id: string;
+  organizations_url: string;
+  received_events_url: string;
+  repos_url: string;
+  site_admin: string;
+  starred_url: string;
+  subscriptions_url: string;
+  type: string;
+  url: string;
+}
+
+interface author_commit {
+  date: string;
+  email: string;
+  name: string;
+}
+
+interface tree_commit {
+  sha: string;
+  url: string;
+}
+
+interface commit {
+  author: author_commit;
+  comment_count: number;
+  committer: author_commit;
+  message: string;
+  tree: tree_commit;
+  url: string;
+  verification: string;
+}
+
+interface commiter {
+  avatar_url: string;
+  events_url: string;
+  followers_url: string;
+  following_url: string;
+  gists_url: string;
+  gravatar_id: string;
+  html_url: string;
+  id: number;
+  login: string;
+  node_id: string;
+  organizations_url: string;
+  received_events_url: string;
+  repos_url: string;
+  site_admin: boolean;
+  starred_url: string;
+  subscriptions_url: string;
+  type: string;
+  url: string;
+}
+
+interface parent {
+  html_url: string;
+  sha: string;
+  url: string;
+}
+
 export default function Home() {
 
-  const [commits, setCommits] = useState([]);
+  const [commits, setCommits] = useState<response_data[]>(
+    [],
+  );
 
   const loadIssues = async () => {
     const response = await octokit.request("GET /repos/{owner}/{repo}/commits", {
       owner: "Kevinbriceo567",
       repo: "git-api-nextjs",
     });
-    setCommits(response.data);
+    const apiData = response.data;
+    console.log(apiData);
+
+    setCommits(apiData);
+
+    /*
+    setCommits(prevCommits => [
+      ...prevCommits,
+      {html_url: 'Bob', node_id: 'Bob', url: 'Bob'},
+    ]);
+    */
+
     console.log("Commits", commits);
   }
 
   useEffect(() => {
-    console.log("OK");
     loadIssues();
   }, [])
 
@@ -65,6 +158,21 @@ export default function Home() {
           height={37}
           priority
         />
+      </div>
+
+      <div>
+        <ul>
+        {commits.map((commit, index) => {
+        return (
+          <div key={index}>
+            <h2>
+              Author: {commit.commit.author.name} / name: {commit.commit.author.email}
+            </h2>
+          </div>
+        );
+      })}
+        </ul>
+
       </div>
 
       <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
